6e5a805cfd9042deb37bade88e54b121a1ba745f
Mawuli2026 <mawuliayikpa89@gmail.com>
2025-06-04 15:09:52 +0100

    remove node_modules

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,4 +7,5 @@ router.get('/', userController.getUsers);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
+
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const express = require('passport')
 
 // Swagger setup
 const swaggerUi = require('swagger-ui-express');
